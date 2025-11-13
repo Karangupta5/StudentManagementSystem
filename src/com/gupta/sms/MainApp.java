8baa5b1ee5f0241bab46e3ac8bb618ea47146172
Karan <karangupta0503@gmail.com>
2025-11-13 12:22:55 +0530

    feat: add Student model with constructors,getters,setters,toString.

--- src/com/gupta/sms/MainApp.java
+++ src/com/gupta/sms/MainApp.java
@@ -2,6 +2,10 @@ package com.gupta.sms;
 
 public class MainApp {
 	public static void main(String args[]) {
-	System.out.println("WELCOME!");
-}
+		Student s1=new Student("Karan",22,"B.Tech");
+		Student s2=new Student("Ritik",24,"MCA");
+		
+		System.out.println("First Student is"+ s1.toString());
+		System.out.println("First Student is"+ s2.toString());
+	}
 }
--- src/com/gupta/sms/Student.java
+++ src/com/gupta/sms/Student.java
@@ -1,5 +1,48 @@
 package com.gupta.sms;
 
 public class Student {
+	private int age,roll_no;
+	private String name,course;
+	
+	private static int counter=1;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name,int age,String course) {
+		this.age=age;
+		this.course=course;
+		this.roll_no=counter++;
+		this.name=name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getRoll_no() {
+		return roll_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
 
+	@Override
+	public String toString() {
+		return " [roll_no=" + roll_no + ", name=" + name + ", age=" + age + ", course=" + course + "]";
+	}
+	
+	
 }
